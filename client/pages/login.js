884743226807bc3c0ae543ba60b0e80120b6cb1a
ArthurBacci64 <arthurbacci64@gmail.com>
2020-07-28 20:18:57 -0300

    Integraçao com o login

--- client/pages/login.js
+++ client/pages/login.js
@@ -1,3 +1,4 @@
+const { ipcRenderer } = require('electron')
 const label = [document.getElementById("nicknameLabel"), document.getElementById("passwordLabel")]
 const input = [document.getElementById("nickname"), document.getElementById("password")]
 
@@ -13,4 +14,11 @@ function labelBlur(id) {
     }
 }
 
+function logIn() {
+    console.log(ipcRenderer.sendSync("login", {
+        name: input[0].value,
+        password: input[1].value,
+        operation: 0
+    }))
+}
 
