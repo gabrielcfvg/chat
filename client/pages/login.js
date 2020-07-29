9e94281f62fe304e07b2b203d66e4a7acbfed4dc
ArthurBacci64 <arthurbacci64@gmail.com>
2020-07-29 16:39:40 -0300

    Mensagens de erro na pagina de login

--- client/pages/login.js
+++ client/pages/login.js
@@ -1,6 +1,7 @@
 const { ipcRenderer } = require('electron')
 const label = [document.getElementById("nicknameLabel"), document.getElementById("passwordLabel")]
 const input = [document.getElementById("nickname"), document.getElementById("password")]
+const error = [document.getElementById("nicknameError"), document.getElementById("passwordError")]
 
 function labelFocus(id) {
     let obj = label[id]
@@ -15,10 +16,30 @@ function labelBlur(id) {
 }
 
 function logIn() {
-    console.log(ipcRenderer.sendSync("login", {
+    returnValue = ipcRenderer.sendSync("login", {
         name: input[0].value,
         password: input[1].value,
         operation: 0
-    }))
+    });
+    switch (returnValue) {
+        case 0:
+            location.href="main.html";
+            break;
+        case 1:
+            error[1].innerHTML = "Senha incorreta";
+            error[0].classList.remove("visible")
+            error[1].classList.add("visible");
+            break;
+        case 2:
+            error[0].innerHTML = "O usuário não existe";
+            error[0].classList.add("visible");
+            error[1].classList.remove("visible");
+            break;
+    }
 }
+
+function noVisible(id) {
+    error[id].classList.remove("visible");
+}
+
 
