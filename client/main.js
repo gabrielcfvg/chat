754bd0a1e4de007727c4776a1ffb8008b9c865de
gabrielcfvg <56398419+gabrielcfvg@users.noreply.github.com>
2020-07-31 16:39:34 -0300

    adicionadas funções de envio e recebimento de mensagens ao client

--- client/main.js
+++ client/main.js
@@ -70,6 +70,19 @@ var dados = {
 let login_ready = false;
 let login_res;
 let login_err = false
+let function_received = false;
+function MessageFunction() {};
+
+ipcMain.on('login', login);
+ipcMain.on("send_message", send_message);
+ipcMain.on("MessageFunction", (event, arg) => {
+
+    MessageFunction = function(message) {
+        arg(message);
+    };
+    function_received = true;
+});
+
 
 
 socket.connect({ host: IP, port: PORTA }, () => {
@@ -77,62 +90,29 @@ socket.connect({ host: IP, port: PORTA }, () => {
     //socket.write('{"type": 2}');
 });
 
-
 socket.on('error', () => {
     login_err = true;
-})
+});
 
 socket.on('data', data => {
 
-    if (!data) {
-        return;
-    }
-
-    let pacote = JSON.parse(data.toString());
-
     if (!login_status) {
-
-        if (pacote.type === 0) {
-
-            // decodificação da chave pública de bae64 pra bytes
-            let chave = new Buffer.from(pacote.content, "base64").toString();
-
-            // criação do pacote a ser enviado para o servidor
-            let saida = {
-                type: 0,
-                content: dados
-            };
-
-            // transformação do pacote em uma string JSON
-            saida = encoder.encode(JSON.stringify(saida));
-
-            // envio do pacote para o servidor
-            socket.write(crypto.publicEncrypt({ key: chave, padding: crypto.constants.RSA_PKCS1_PADDING }, saida));
-
-        }
-        else if (pacote.type === 1) {
-
-            if (pacote.content === 0) {
-                login_status = true;
-            }
-
-            login_ready = true;
-            login_res = pacote.content;
-
-
-            console.log(">>>" + pacote.content.toString());
-
-        }
+        socket_login(data);
     }
-
+    else {
+        socket_client(data);
+    }
 });
+
+
+
+
 
 function sleep(ms) {
     return new Promise(resolve => {
         setTimeout(() => resolve(true), ms);
     });
 }
-
 
 async function login(event, arg) {
 
@@ -152,4 +132,100 @@ async function login(event, arg) {
     event.returnValue = login_res;
 }
 
-ipcMain.on('login', login);
+function socket_login(data) {
+    if (!data){
+        return;
+    }
+
+    let pacote = JSON.parse(data.toString());
+
+    if (!login_status) {
+
+        if (pacote.type === 0) {
+
+            // decodificação da chave pública de bae64 pra bytes
+            let chave = new Buffer.from(pacote.content, "base64").toString();
+            
+            // criação do pacote a ser enviado para o servidor
+            let saida = {
+                type: 0,
+                content: dados
+            };
+
+            // transformação do pacote em uma string JSON
+            saida = encoder.encode(JSON.stringify(saida));
+
+            // envio do pacote para o servidor
+            socket.write(crypto.publicEncrypt({ key: chave, padding: crypto.constants.RSA_PKCS1_PADDING }, saida));
+
+        }
+        else if (pacote.type === 1) {
+
+                if (pacote.content === 0) {
+                    login_status = true;
+                    socket.write('{"type": 20, "content": 1}')
+                }
+
+                login_ready = true;
+                login_res = pacote.content;
+
+            
+            console.log(">>>" + pacote.content.toString());
+
+        }
+    }
+}
+
+function socket_client(data) {
+
+    console.log("pacote recebido");
+    if (!data) {
+        return;
+    }
+
+    for(raw_package of data.toString().split("|")) {
+
+        if (!raw_package) {
+            continue;
+        }
+
+        let package = JSON.parse(raw_package);
+
+        switch (package.type) {
+
+            case 10: {
+                
+                let new_message = {
+                    autor: package.content.autor,
+                    message: package.content.message
+                };
+    
+                if (function_received) {
+                    MessageFunction(new_message);
+                }
+
+                console.log(new_message.autor);
+                console.log(new_message.message);
+                console.log("-----------------------");
+            }
+        }
+    }
+}
+
+function send_message(event, arg) {
+
+    if (!login_status) {
+        return;
+    }
+
+    let package = {
+        
+        type: 10,
+        content: {
+            channel: arg.channel,
+            message: arg.message,
+        }
+    }
+
+    socket.write(JSON.stringify(package));
+}
